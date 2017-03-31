7b11d97f7e3efd6c934e5efe2b1fb3ca2566d2e5
winthos <winson.whan@gmail.com>
2017-03-31 11:14:16 -0700

    KARS WAMMU ESIDISI

---
+++ Assets/CarController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        //default state is busted and old
+        if (col.gameObject.tag == "RewindBomb")
+        {
+            if(gameObject.GetComponentInChildren<Animator>().GetInteger("State") == 0)
+            {
+                gameObject.GetComponentInChildren<Animator>().SetTrigger("ToNewCar");
+                gameObject.GetComponentInChildren<Animator>().SetInteger("State", 1);
+            }
+        }
+
+        if(col.gameObject.tag == "FastForwardBomb")
+        {
+            if (gameObject.GetComponentInChildren<Animator>().GetInteger("State") == 1)
+            {
+                gameObject.GetComponentInChildren<Animator>().SetTrigger("ToOldCar");
+                gameObject.GetComponentInChildren<Animator>().SetInteger("State", 0);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CabinController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CabinController : MonoBehaviour
+{
+    public GameObject WhereIsTheCabin;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        print("something touched me");
+        if (col.gameObject.tag == "RewindBomb")
+        {
+            //if we are the destroyed cabin
+            if (WhereIsTheCabin.GetComponent<Animator>().GetInteger("State") == 0)
+            {
+                print("fast forward");
+                WhereIsTheCabin.GetComponent<Animator>().SetTrigger("ToNewCabin");
+                WhereIsTheCabin.GetComponent<Animator>().SetInteger("State", 1);
+            }
+
+        }
+
+        if (col.gameObject.tag == "FastForwardBomb")
+        {
+            //if we are the new cabin
+            if (WhereIsTheCabin.GetComponent<Animator>().GetInteger("State") == 1)
+            {
+                print("rewind");
+                WhereIsTheCabin.GetComponent<Animator>().SetTrigger("ToDestroyedCabin");
+                WhereIsTheCabin.GetComponent<Animator>().SetInteger("State", 0);
+            }
+
+        }
+    }
+}
+    
