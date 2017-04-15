3e54c78b0499805e5e8168f5280e8df0977f1386
winthos <winson.whan@gmail.com>
2017-04-15 14:23:14 -0700

    quit stuff added, cheats added

---
+++ Assets/ButtonLogic.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonLogic : MonoBehaviour 
+{
+    public Canvas QuitScreen;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //make appear
+	    if(Input.GetKeyDown(KeyCode.Escape) && QuitScreen.GetComponent<CanvasGroup>().interactable == false)
+        {
+            QuitScreen.GetComponent<CanvasGroup>().alpha = 1;
+            QuitScreen.GetComponent<CanvasGroup>().interactable = true;
+            QuitScreen.GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Escape) && QuitScreen.GetComponent<CanvasGroup>().interactable == true)
+        {
+            QuitScreen.GetComponent<CanvasGroup>().alpha = 0;
+            QuitScreen.GetComponent<CanvasGroup>().interactable = false;
+            QuitScreen.GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+
+        if(QuitScreen.GetComponent<CanvasGroup>().interactable == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                QuitGame();
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/CheatMove.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheatMove : MonoBehaviour 
+{
+    public GameObject pos1;
+
+    public GameObject pos2;
+
+    public GameObject pos3;
+
+    public GameObject pos4;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetKeyDown("1"))
+        {
+            gameObject.transform.position = pos1.transform.position;
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            gameObject.transform.position = pos2.transform.position;
+        }
+
+        if (Input.GetKeyDown("3"))
+        {
+            gameObject.transform.position = pos3.transform.position;
+        }
+
+        if (Input.GetKeyDown("4"))
+        {
+            gameObject.transform.position = pos4.transform.position;
+        }
+    }
+}
